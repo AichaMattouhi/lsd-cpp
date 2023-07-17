4769d1ec8b9ba91a0c50c0bb34b867dbf5dbe140
aichaMattouhi <aicha165mattouhi@gmail.com>
2023-07-17 12:26:51 +0100

    deuxieme version

--- ses2/exo2TP2.c
+++ ses2/exo2TP2.c
@@ -1,64 +1,65 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int **dummy(int**x,int n)
+int **dummmyb(int *x, int n)
 {
     int **y;
     int i;
     int j;
-    i=0;
-    y=(int**)malloc(sizeof (int*)*(n+1));
-    while (i<n)
+    i = 0;
+    y = (int **)malloc(sizeof(int *) * (n + 1));
+    while (i < n)
     {
-        y[i]=(int*)malloc(sizeof(int)*n);
-        j=0;
-        while (j<n)
+        y[i] = (int *)malloc(sizeof(int) * n);
+        j = 0;
+        while (j < n)
         {
-            y[i][j]=j;
+            y[i][j] = j;
             j++;
         }
         i++;
     }
-    y[n]=x[0];
-    for(int e = 1; e < n; e++) free(x[e]);
+    y[n] = x[0];
+    for (int e = 1; e < n; e++)
+        free(x[e]);
     free(x);
     return y;
 }
 
-
 int main()
 {
     int n, i, j;
-    int** x;
-    n=10;
-    i=0;
-    x=(int**)malloc(sizeof(int*)* n);
-    while(i<n)
+    int **x;
+    n = 10;
+    i = 0;
+    x = (int **)malloc(sizeof(int *) * n);
+    while (i < n)
     {
-        x[i]=(int*)malloc(sizeof(int)*n);
-        j=0;
-        while(j<n)
+        x[i] = (int *)malloc(sizeof(int) * n);
+        j = 0;
+        while (j < n)
         {
-            x[i][j]=j;
+            x[i][j] = j;
             j++;
         }
         i++;
     }
-    x=dummy(x,n);
-    i=0;
-    while(i<n+1)
+    x = dummy(x, n);
+    i = 0;
+    while (i < n + 1)
     {
-        j=0;
-        while(j<n)
+        j = 0;
+        while (j < n)
         {
-            printf("%d ",x[i][j]);
+            printf("%d ", x[i][j]);
             j++;
         }
         printf("\n");
         i++;
     }
-    for(int e = 0; e <= n; e++) free(x[e]);
+    for (int e = 0; e <= n; e++)
+        free(x[e]);
     free(x);
-    
+
     return 0;
 }
